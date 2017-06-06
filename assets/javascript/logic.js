c5dce856e68658dfa715d9a0ba63a46f4c6d4638
Jon Berry <jonpber@gmail.com>
2017-06-06 08:19:40 -0700

    trying to fix sign in

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -15,6 +15,29 @@ $(function(){
 	var googleProvider = new firebase.auth.GoogleAuthProvider();
 	var gitHubProvider = new firebase.auth.GithubAuthProvider();
 
+	firebase.auth().getRedirectResult().then(function(result) {
+	  if (result.credential) {
+	    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+	    var token = result.credential.accessToken;
+	    $(".mainContent").css("display", "block");
+    	updateBoard();
+		setInterval(updateBoard, 60000);
+	    // ...
+	  }
+		  // The signed-in user info.
+		  var user = result.user;
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  $(".signInBlock").css("display", "block");
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  // ...
+		});
+
 	firebase.auth().onAuthStateChanged(function(user) {
 		  if (user) {
 		    $(".mainContent").css("display", "block");
